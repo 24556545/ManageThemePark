067984fd90aa9746456fbed1f098d073aa20fbba
24556545 <g.senapathi.10@student.scu.edu.au>
2024-08-06 16:34:30 +0530

    defined all classes with constructors and getters and setters

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -1,5 +1,20 @@
 public class AssignmentTwo {
     public static void main(String[] args) {
         System.out.println("Hello !");
+
+
+    }
+    public void partThree(){
+    }
+    public void partFourA(){
+    }
+    public void partFourB(){
+    }
+    public void partFive(){
+    }
+    public void partSix(){
+    }
+    public void partSeven(){
+
     }
 }
--- src/Ride.java
+++ src/Ride.java
@@ -1,2 +1,45 @@
 public class Ride {
+    private String rideName;
+    private int duration;
+    private Employee operator;
+
+    // Default constructor
+    public Ride() {
+
+    }
+
+    public Ride(String rideName, int duration, Employee operator) {
+        this.rideName = rideName;
+        this.duration = duration;
+        this.operator = operator;
+    }
+
+    // Getters and Setters
+    public String getRideName() {
+        return rideName;
+    }
+
+    public void setRideName(String rideName) {
+        this.rideName = rideName;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public Employee getOperator() {
+        return operator;
+    }
+
+    public void setOperator(Employee operator) {
+        this.operator = operator;
+    }
+
+    public void assignOperator(Employee operator) {
+        this.operator = operator;
+    }
 }
